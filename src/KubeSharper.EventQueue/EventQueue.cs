b4902a4065a7bf85895bcef9067bb3ecbaebfe41
Valer Orlovsky <valer.orlovsky@centrica.com>
2020-03-30 12:08:32 +0200

    Switch eventqueue to non-concurrent structures with locks

--- src/KubeSharper.EventQueue/EventQueue.cs
+++ src/KubeSharper.EventQueue/EventQueue.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
@@ -8,29 +9,66 @@ namespace KubeSharper.EventQueue
 {
     public class EventQueue<T> : IEventQueue<T> 
     {
-        private readonly ConcurrentQueue<T> _items = new ConcurrentQueue<T>();
-        private readonly ConcurrentDictionary<T, bool> _set = new ConcurrentDictionary<T, bool>();
+        private readonly Queue<T> _items = new Queue<T>();
+        private readonly Dictionary<T, byte> _set = new Dictionary<T, byte>();
+        private static readonly object _lock = new object();
 
 
         public int Count => _items.Count;
 
         public async Task<bool> TryAdd(T item)
         {
-            if(_set.TryAdd(item, true))
+            Log.Debug($"Trying to add {item}");
+            lock (_lock)
             {
-                _items.Enqueue(item);
-                return true;
-            }
-            else
-            {
+                if (SetAdd(item))
+                {
+                    _items.Enqueue(item);
+                    Log.Debug($"{item} enqueued");
+                    return true;
+                }
                 return false;
             }
         }
 
         public bool TryGet(out T item)
         {
-            return _items.TryDequeue(out item);
+            lock(_lock)
+            {
+                if(_items.Count == 0)
+                {
+                    item = default;
+                    return false;
+                }
+
+                var dequeued = _items.Dequeue();
+                Log.Debug($"Dequeued {dequeued}");
+                
+                Log.Debug($"Removing {dequeued} from set");
+                SetRemove(dequeued);
+
+                item = dequeued;
+                return true;
+            }
         }
 
+        private bool SetAdd(T item)
+        {
+            if (_set.ContainsKey(item))
+            {
+                Log.Debug($"{item} already in set");
+                return false;
+            }
+            _set.Add(item, 1);
+            Log.Debug($"{item} added to set");
+            return true;
+        }
+        private void SetRemove(T item)
+        {
+            if(_set.ContainsKey(item))
+            {
+                _set.Remove(item);
+            }
+        }
     }
 }
